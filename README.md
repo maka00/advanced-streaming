796e9f11b672cabb34c922a15c214f849096f82a
maka00 <34419221+maka00@users.noreply.github.com>
2021-11-27 17:17:49 +0100

    add destroy/create memory test

--- README.md
+++ README.md
@@ -4,10 +4,53 @@
 
 ## multipipe
 
-Analyse the memory usage of various ways to start/stop a pipeline. Context: Two pipelines are running at the same time
+Analyse the memory usage of various ways to start/stop a pipeline.
+
+Context: Two pipelines are running at the same time
 (which is ok for gstreamer)
 
+One pipeline is constantly running, started on a separate thread:
+![pipeline one](docs/multipipe_pipeline_one.png "pipeline one")
+
+The second pipeline:
+![pipeline one](docs/multipipe_pipeline_two.png "pipeline one")
+
+* The second pipeline is started/stopped via set state constantly (multipipe -s)
+* The second pipeline is created/started/stopped/destroyed via set state and gst_object_unref constantly (multipipe -c)
+* The second pipeline is running constantly (multipipe -r)
+
+```/bin/bash
+> ./multipipe -h
+[main.cpp][main][97] Application started
+Analyze memory consumption of gstreamer start/stop pipes
+Usage:
+  multipipe [OPTION...]
+
+  -s, --start     starting and stopping
+  -c, --creating  starting and stopping by destroying and creating 
+                  pipelines
+  -r, --running   constantly running pipelines
+  -h, --help      Print usage
+
+use GST_DEBUG_DUMP_DOT_DIR=. to export the pipeline structure.
+ and dot -Tpng filename.dot -o outfile.png
+
+```
+
 ### Results
+
+```/bin/bash
+[main.cpp][main][97] Application started
+[main.cpp][main][146] Starting/Stopping with destroying the pipeline
+[main.cpp][check_mem_usage][27] [Started] RSS: 305272kB   | SM: 16184kB   | PM: 289088kB  | Max: 289088kB
+[main.cpp][check_mem_usage][27] [Stopped] RSS: 31416kB    | SM: 16376kB   | PM: 15040kB   | Max: 289088kB
+[main.cpp][check_mem_usage][27] [Started] RSS: 298276kB   | SM: 16376kB   | PM: 281900kB  | Max: 289088kB
+[main.cpp][check_mem_usage][27] [Stopped] RSS: 56864kB    | SM: 16376kB   | PM: 40488kB   | Max: 289088kB
+[main.cpp][check_mem_usage][27] [Started] RSS: 311392kB   | SM: 16376kB   | PM: 295016kB  | Max: 295016kB
+[main.cpp][check_mem_usage][27] [Stopped] RSS: 74272kB    | SM: 16376kB   | PM: 57896kB   | Max: 295016kB
+[main.cpp][check_mem_usage][27] [Started] RSS: 315392kB   | SM: 16376kB   | PM: 299016kB  | Max: 299016kB
+[main.cpp][check_mem_usage][27] [Stopped] RSS: 81208kB    | SM: 16376kB   | PM: 64832kB   | Max: 299016kB
+```
 
 There is a memory leak when one pipeline is stopped and then freed (via gst_object_unref). The expectation was that the
 memory is freed completely after the gst_object_unref. Looks like the memory is still held in the mainloop.
--- multipipe/main.cpp
+++ multipipe/main.cpp
@@ -6,6 +6,7 @@
 #include <fmt/format.h>
 #include <gst/gst.h>
 #include <thread>
+#include <cxxopts.hpp>
 
 void exit_handler(int s) {
     SPDLOG_INFO("Got Signal {}", s);
@@ -30,10 +31,79 @@ gpointer main_loop_runner(gpointer ptr) {
     g_main_loop_run(reinterpret_cast<GMainLoop *>(ptr));
 }
 
+void start_stop_test(const std::string &launch_cmd) {
+    GError *err{nullptr};
+    auto pipeline2 = gst_parse_launch_full(launch_cmd.c_str(), nullptr, GST_PARSE_FLAG_FATAL_ERRORS, &err);
+    if (err) {
+        SPDLOG_ERROR("{}", err->message);
+        g_error_free(err);
+    }
+    GST_DEBUG_BIN_TO_DOT_FILE(GST_BIN(pipeline2), GST_DEBUG_GRAPH_SHOW_STATES, "pipeline_two");
+    std::thread([pipeline2] {
+        while (true) {
+            gst_element_set_state(pipeline2, GST_STATE_PLAYING);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            check_mem_usage("Started");
+            gst_element_set_state(pipeline2, GST_STATE_NULL);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            check_mem_usage("Stopped");
+        };
+    }).detach();
+}
+
+void create_destroy_test(const std::string &launch_cmd) {
+    GError *err{nullptr};
+    std::thread([launch_cmd, &err] {
+        while (true) {
+            auto pipeline2 = gst_parse_launch_full(launch_cmd.c_str(), nullptr, GST_PARSE_FLAG_FATAL_ERRORS, &err);
+            GST_DEBUG_BIN_TO_DOT_FILE(GST_BIN(pipeline2), GST_DEBUG_GRAPH_SHOW_STATES, "pipeline_two");
+            if (err) {
+                SPDLOG_ERROR("{}", err->message);
+                g_error_free(err);
+            }
+            gst_element_set_state(pipeline2, GST_STATE_PLAYING);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            check_mem_usage("Started");
+            gst_element_set_state(pipeline2, GST_STATE_NULL);
+            gst_object_unref(pipeline2);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            check_mem_usage("Stopped");
+        };
+    }).detach();
+}
+
+void constant_running_test(const std::string &launch_cmd) {
+    GError *err{nullptr};
+    auto pipeline2 = gst_parse_launch_full(launch_cmd.c_str(), nullptr, GST_PARSE_FLAG_FATAL_ERRORS, &err);
+    if (err) {
+        SPDLOG_ERROR("{}", err->message);
+        g_error_free(err);
+    }
+    GST_DEBUG_BIN_TO_DOT_FILE(GST_BIN(pipeline2), GST_DEBUG_GRAPH_SHOW_STATES, "pipeline_two");
+    std::thread([] {
+        while (true) {
+            check_mem_usage("");
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        };
+    }).detach();
+    std::thread([pipeline2] {
+        gst_element_set_state(pipeline2, GST_STATE_PLAYING);
+    }).detach();
+}
+
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
     static auto console = spdlog::stdout_color_mt("console");
     spdlog::set_pattern("[%H:%M:%S.%e][%s][%!][%#] %v");
     SPDLOG_INFO("Application started");
+
+    cxxopts::Options options("multipipe", "Analyze memory consumption of gstreamer start/stop pipes");
+
+    options.add_options()
+            ("s,start", "starting and stopping")
+            ("c,creating", "starting and stopping by destroying and creating pipelines")
+            ("r,running", "constantly running pipelines")
+            ("h,help", "Print usage");
+    auto result = options.parse(argc, argv);
     struct sigaction sigIntHandler;
     sigIntHandler.sa_handler = exit_handler;
     sigemptyset(&sigIntHandler.sa_mask);
@@ -55,6 +125,7 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
         assert(false);
     }
     gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    GST_DEBUG_BIN_TO_DOT_FILE(GST_BIN(pipeline), GST_DEBUG_GRAPH_SHOW_STATES, "pipeline_one");
 
     const std::string launch_cmd = ""
                                    "videotestsrc is-live=true pattern=11 "
@@ -68,58 +139,21 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
                                    //"t. ! queue ! x264enc tune=zerolatency ! video/x-h264, stream-format=byte-stream ! fakesink"
                                    "";
 
-
-//#define START_STOP_TEST
-#define CREATE_DESTROY_TEST
-//#define CONST_RUNNING_TEST
-#ifdef START_STOP_TEST
-    auto pipeline2 = gst_parse_launch_full(launch_cmd.c_str(), nullptr, GST_PARSE_FLAG_FATAL_ERRORS, &err);
-    if (err) {
-        SPDLOG_ERROR("{}", err->message);
-        g_error_free(err);
+    if (result["s"].count()) {
+        SPDLOG_INFO("Starting/Stopping without destroying the pipeline");
+        start_stop_test(launch_cmd);
+    } else if (result["c"].count()) {
+        SPDLOG_INFO("Starting/Stopping with destroying the pipeline");
+        create_destroy_test(launch_cmd);
+    } else if (result["r"].count()) {
+        SPDLOG_INFO("constant running the pipeline");
+        constant_running_test(launch_cmd);
+    } else {
+        std::cout << options.help() << std::endl;
+        std::cout << "use GST_DEBUG_DUMP_DOT_DIR=. to export the pipeline structure." << std::endl;
+        std::cout << " and dot -Tpng filename.dot -o outfile.png" << std::endl;
+        std::exit(0);
     }
-    std::thread([pipeline2] {
-        while (true) {
-            gst_element_set_state(pipeline2, GST_STATE_PLAYING);
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            check_mem_usage("Started");
-            gst_element_set_state(pipeline2, GST_STATE_NULL);
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            check_mem_usage("Stopped");
-        };
-    }).detach();
-#endif
-#ifdef CREATE_DESTROY_TEST
-    std::thread([launch_cmd, &err] {
-        while (true) {
-            auto pipeline2 = gst_parse_launch_full(launch_cmd.c_str(), nullptr, GST_PARSE_FLAG_FATAL_ERRORS, &err);
-            gst_element_set_state(pipeline2, GST_STATE_PLAYING);
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            check_mem_usage("Started");
-            gst_element_set_state(pipeline2, GST_STATE_NULL);
-            gst_object_unref(pipeline2);
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            check_mem_usage("Stopped");
-        };
-    }).detach();
-#endif
-#if CONST_RUNNING_TEST
-    auto pipeline2 = gst_parse_launch_full(launch_cmd.c_str(), nullptr, GST_PARSE_FLAG_FATAL_ERRORS, &err);
-    if (err) {
-        SPDLOG_ERROR("{}", err->message);
-        g_error_free(err);
-    }
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    std::thread([] {
-        while (true) {
-            check_mem_usage("");
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        };
-    }).detach();
-    std::thread([pipeline2] {
-        gst_element_set_state(pipeline2, GST_STATE_PLAYING);
-    }).detach();
-#endif
 
 
     pause();
