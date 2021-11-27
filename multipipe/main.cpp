2c8d11ec78a52d666d25e793b17a8a2482f91480
maka00 <34419221+maka00@users.noreply.github.com>
2021-11-27 12:31:07 +0100

    add destroy/create memory test

--- multipipe/main.cpp
+++ multipipe/main.cpp
@@ -12,7 +12,7 @@ void exit_handler(int s) {
     std::exit(EXIT_SUCCESS);
 }
 
-void check_mem_usage() {
+void check_mem_usage(const std::string &info) {
     static double max_pm{0.0};
     int tSize = 0, resident = 0, share = 0;
     std::ifstream buffer("/proc/self/statm");
@@ -23,7 +23,7 @@ void check_mem_usage() {
     double rss = resident * page_size_kb;
     double shared_mem = share * page_size_kb;
     max_pm = std::max(max_pm, rss - shared_mem);
-    SPDLOG_INFO("RSS: {}kB | SM: {}kB | PM: {}kB | Max: {}kB", rss, shared_mem, rss - shared_mem, max_pm);
+    SPDLOG_INFO("[{}] RSS: {}kB\t| SM: {}kB\t| PM: {}kB\t| Max: {}kB", info, rss, shared_mem, rss - shared_mem, max_pm);
 }
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
@@ -46,42 +46,66 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) {
         SPDLOG_ERROR("{}", err->message);
         g_error_free(err);
     }
-    auto pipeline2 = gst_parse_launch_full(//
-            "videotestsrc is-live=true pattern=11 "
-            //"! video/x-raw,width=1280,height=720 "
-            "! video/x-raw,widht=3840,height=2160 "
-            //"v4l2src device=/dev/video0 "
-            "! videoconvert ! tee name=t "
-            "t. ! queue ! fakesink "
-            "t. ! queue ! x264enc tune=zerolatency ! video/x-h264, stream-format=byte-stream ! h264parse ! mpegtsmux ! fakesink"
-            //"t. ! queue ! x264enc tune=zerolatency ! video/x-h264, stream-format=byte-stream ! fakesink"
-            "",
-            nullptr,
-            GST_PARSE_FLAG_FATAL_ERRORS, &err);
+    const std::string launch_cmd = ""
+                                   "videotestsrc is-live=true pattern=11 "
+                                   //"! video/x-raw,width=1280,height=720 "
+                                   "! video/x-raw,widht=3840,height=2160 "
+                                   //"v4l2src device=/dev/video0 "
+                                   "! videoconvert ! tee name=t "
+                                   "t. ! queue ! x264enc tune=zerolatency ! video/x-h264, stream-format=byte-stream ! h264parse ! splitmuxsink muxer=mpegtsmux location=./test_0_%02d.ts max-size-time=10000000000 "
+                                   //"t. ! queue ! x264enc tune=zerolatency ! video/x-h264, stream-format=byte-stream ! h264parse ! mpegtsmux ! fakesink"
+                                   "t. ! queue ! x264enc tune=zerolatency ! video/x-h264, stream-format=byte-stream ! h264parse ! splitmuxsink muxer=mpegtsmux location=./test_1_%02d.ts max-size-time=10000000000 "
+                                   //"t. ! queue ! x264enc tune=zerolatency ! video/x-h264, stream-format=byte-stream ! fakesink"
+                                   "";
+
+
+//#define START_STOP_TEST
+#define CREATE_DESTROY_TEST
+//#define CONST_RUNNING_TEST
+#ifdef START_STOP_TEST
+    auto pipeline2 = gst_parse_launch_full(launch_cmd.c_str(), nullptr, GST_PARSE_FLAG_FATAL_ERRORS, &err);
     if (err) {
         SPDLOG_ERROR("{}", err->message);
         g_error_free(err);
     }
-
-    gst_element_set_state(pipeline, GST_STATE_PLAYING);
-    std::thread([] {
-        while (true) {
-            check_mem_usage();
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-        };
-    }).detach();
-//#define START_STOP_TEST
-#ifdef START_STOP_TEST
     std::thread([pipeline2] {
         while (true) {
             gst_element_set_state(pipeline2, GST_STATE_PLAYING);
             std::this_thread::sleep_for(std::chrono::seconds(1));
+            check_mem_usage("Started");
             gst_element_set_state(pipeline2, GST_STATE_NULL);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            check_mem_usage("Stopped");
+        };
+    }).detach();
+#endif
+#ifdef CREATE_DESTROY_TEST
+    std::thread([launch_cmd, &err] {
+        while (true) {
+            auto pipeline2 = gst_parse_launch_full(launch_cmd.c_str(), nullptr, GST_PARSE_FLAG_FATAL_ERRORS, &err);
+            gst_element_set_state(pipeline2, GST_STATE_PLAYING);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            check_mem_usage("Started");
+            gst_element_set_state(pipeline2, GST_STATE_NULL);
+            gst_object_unref(pipeline2);
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            check_mem_usage("Stopped");
+        };
+    }).detach();
+#endif
+#if CONST_RUNNING_TEST
+    auto pipeline2 = gst_parse_launch_full(launch_cmd.c_str(), nullptr, GST_PARSE_FLAG_FATAL_ERRORS, &err);
+    if (err) {
+        SPDLOG_ERROR("{}", err->message);
+        g_error_free(err);
+    }
+    gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    std::thread([] {
+        while (true) {
+            check_mem_usage("");
             std::this_thread::sleep_for(std::chrono::seconds(1));
         };
     }).detach();
-#else
-
     std::thread([pipeline2] {
         gst_element_set_state(pipeline2, GST_STATE_PLAYING);
     }).detach();
